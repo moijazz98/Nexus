b18a5337f9946bb74f0cea86e89f7c158b556e58
moijazz98 <86704600+moijazz98@users.noreply.github.com>
2024-08-22 09:07:34 -0700

    1st commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler'; // This import must be at the top of the file
+
+import React from 'react';
+import Navigation from './src/navigation';
+
+const App = () => {
+  return <Navigation />;
+};
+
+export default App;
