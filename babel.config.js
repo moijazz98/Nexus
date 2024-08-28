27a84da80ae4475059b85986a0c2eef286ebe818
moijazz98 <mohamedijazz98@gmail.com>
2024-08-28 13:17:17 -0700

    deleted all

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['react-native-reanimated/plugin', {loose: true}],
-    ['@babel/plugin-transform-private-methods', {loose: true}],
-  ],
-};
